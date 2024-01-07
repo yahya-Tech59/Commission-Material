a2ec7b653b98a1b0ca32d3d3c4f437a554efae8b
yahya-Tech59 <yahyekade790@gmail.com>
2024-01-07 15:17:32 +0300

    sidebar well designed

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -3,25 +3,13 @@ import React, { createContext, useState } from "react";
 export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
-
-  const handleOpen = () => {
-    setSidebarOpen(true);
-  };
-
-  const handleClose = () => {
-    setSidebarOpen(false);
-  };
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
 
   const values = {
-    sidebarOpen,
-    handleOpen,
-    handleClose,
     currentPage,
     handlePageChange,
   };
