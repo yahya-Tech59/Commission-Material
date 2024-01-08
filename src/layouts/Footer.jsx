943c22fdaa9506453e259f9f53783bf53a9245a5
yahya-Tech59 <yahyekade790@gmail.com>
2024-01-08 12:35:24 +0300

    footer changed into responsive but not completetly

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,5 +1,5 @@
-import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import { Box, Grid, Typography } from "@mui/material";
 
 export const Footer = () => {
   return (
@@ -7,21 +7,42 @@ export const Footer = () => {
       sx={{
         bgcolor: "white",
         ml: 1,
-        width: "96rem",
-        height: "1.8rem",
+        width: "100%",
         display: "flex",
-        gap: 3,
+        flexDirection: "column",
         p: 3,
         mb: 3,
         boxShadow: "0px 4px 6px rgba(156, 163, 175, 0.15)",
+        display: {
+          xs: { display: "flex", flexDirection: "column", overflow: "hidden" },
+        },
       }}
     >
-      <Typography variant="h6">Privacy Policy</Typography>
-      <Typography variant="h6">Terms of use</Typography>
+      <Grid
+        container
+        spacing={3}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item xs={12} sm={6} md={2}>
+          <Typography variant="h6">Privacy Policy</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} md={2}>
+          <Typography variant="h6">Terms of use</Typography>
+        </Grid>
 
-      <Typography variant="h6" sx={{ ml: "66rem" }}>
-        @ 2022 CawaaleICT
-      </Typography>
+        <Grid item xs={12} sm={6} md={2}>
+          <Typography
+            variant="h6"
+            sx={{
+              textAlign: "center",
+              display: { xs: { position: "relative", right: 800 } },
+            }}
+          >
+            @ 2022 CawaaleICT
+          </Typography>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
