5f3130fca38662263c5c950d5afbb84007479014
yahya-Tech59 <yahyekade790@gmail.com>
2024-01-17 15:55:58 +0300

    dashboard complete responsive on tablet Screen

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -8,19 +8,19 @@ export const Footer = () => {
       bgcolor="white"
       ml={{ md: 1, sm: -6 }}
       width={{ md: "97rem", sm: "45rem" }}
-      p={{ md: 3, sm: 1 }}
+      p={{ md: 2.5, sm: 1 }}
       mb={3}
       boxShadow="0px 4px 6px rgba(156, 163, 175, 0.15)"
     >
       <Box display="flex" gap={{ md: 2, sm: 3 }} px={{ md: 3, sm: 5 }}>
         <Typography variant="h6">Privacy Policy</Typography>
 
-        <Typography variant="h6" sx={{ ml: { sm: 15 } }}>
+        <Typography variant="h6" sx={{ ml: { sm: 5 } }}>
           Terms of use
         </Typography>
       </Box>
 
-      <Typography variant="h6" ml={{ md: "67rem", sm: "8rem" }}>
+      <Typography variant="h6" ml={{ md: "54rem", sm: "8rem" }}>
         @ 2022 CawaaleICT
       </Typography>
     </Box>
