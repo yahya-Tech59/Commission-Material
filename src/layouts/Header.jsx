add900c958e01b6bdf73b168769c9ce800607a84
yahya-Tech59 <yahyekade790@gmail.com>
2024-01-17 15:09:17 +0300

    header processing for responsive

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -14,13 +14,14 @@ import {
 
 export const Header = () => {
   return (
-    <Toolbar
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        backgroundColor: "white",
-        boxShadow: "0px 4px 6px rgba(156, 163, 175, 0.15)",
-      }}
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      backgroundColor="white"
+      boxShadow="0px 4px 6px rgba(156, 163, 175, 0.15)"
+      width="97rem"
+      mt={1}
+      py={1}
     >
       <Box alignItems="center">
         <Button
@@ -44,6 +45,7 @@ export const Header = () => {
           borderColor: "#D9D9D9",
           height: 36,
           ml: 2,
+          mt: 1,
         }}
       />
 
@@ -61,30 +63,30 @@ export const Header = () => {
         <Typography variant="body1">Paid</Typography>
       </Button>
 
-      <Box alignItems="center" ml={125}>
+      <Box display="flex" gap={2} alignItems="center" ml={125} pr={2}>
         <Typography variant="h2" fontSize={18}>
           EN
         </Typography>
+
+        <Icon sx={{ fontSize: 25 }}>
+          <IoMdNotificationsOutline />
+        </Icon>
+
+        <Box border={0.5} borderColor="#D9D9D9" height={36} ml={1} />
+
+        <Typography variant="h2" fontSize="1rem">
+          Jama Abdi
+        </Typography>
+
+        <Icon
+          sx={{
+            color: "primary.main",
+            fontSize: "2rem",
+          }}
+        >
+          <HiMiniUserCircle />
+        </Icon>
       </Box>
-
-      <Icon sx={{ fontSize: 25 }}>
-        <IoMdNotificationsOutline />
-      </Icon>
-
-      <Box border={0.5} borderColor="#D9D9D9" height={36} ml={2} />
-
-      <Typography variant="h2" fontSize="1rem">
-        Jama Abdi
-      </Typography>
-
-      <Icon
-        sx={{
-          color: "primary.main",
-          fontSize: "2rem",
-        }}
-      >
-        <HiMiniUserCircle />
-      </Icon>
-    </Toolbar>
+    </Box>
   );
 };
