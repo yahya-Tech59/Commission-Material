53f6bb10839753d822891a89cf395b9edd09321a
yahya-Tech59 <yahyekade790@gmail.com>
2024-01-17 15:18:57 +0300

    header responsive on sm screen

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,15 +2,7 @@ import React from "react";
 import { HiMiniUserCircle } from "react-icons/hi2";
 import { GoHomeFill } from "react-icons/go";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import {
-  Box,
-  Button,
-  Grid,
-  Hidden,
-  Icon,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Icon, Typography } from "@mui/material";
 
 export const Header = () => {
   return (
@@ -19,11 +11,11 @@ export const Header = () => {
       justifyContent="space-between"
       backgroundColor="white"
       boxShadow="0px 4px 6px rgba(156, 163, 175, 0.15)"
-      width="97rem"
+      width={{ md: "97rem", sm: "43rem" }}
       mt={1}
       py={1}
     >
-      <Box alignItems="center">
+      <Box display="flex" alignItems="center">
         <Button
           sx={{
             display: "flex",
@@ -37,33 +29,42 @@ export const Header = () => {
           </Icon>
           <Typography variant="body1">Home</Typography>
         </Button>
+
+        <Box
+          sx={{
+            border: 0.5,
+            borderColor: "#D9D9D9",
+            height: 36,
+            ml: 2,
+            mt: 1,
+          }}
+        />
+
+        <Button
+          sx={{
+            display: "flex",
+            ":hover": {
+              borderBottom: { xs: "1px solid #3A57E8", md: "none" },
+            },
+          }}
+        >
+          <Icon sx={{ fontSize: 26, mb: 1, width: "2.2rem", height: "2rem" }}>
+            <GoHomeFill />
+          </Icon>
+          <Typography variant="body1">Paid</Typography>
+        </Button>
       </Box>
 
       <Box
-        sx={{
-          border: 0.5,
-          borderColor: "#D9D9D9",
-          height: 36,
-          ml: 2,
-          mt: 1,
-        }}
-      />
-
-      <Button
-        sx={{
-          display: "flex",
-          ":hover": {
-            borderBottom: { xs: "1px solid #3A57E8", md: "none" },
-          },
-        }}
+        display="flex"
+        gap={2}
+        alignItems="center"
+        ml={125}
+        pr={2}
+        position={{ sm: "absolute" }}
+        right={{ sm: "2rem" }}
+        px={{ sm: 2 }}
       >
-        <Icon sx={{ fontSize: 26, mb: 1, width: "2.2rem", height: "2rem" }}>
-          <GoHomeFill />
-        </Icon>
-        <Typography variant="body1">Paid</Typography>
-      </Button>
-
-      <Box display="flex" gap={2} alignItems="center" ml={125} pr={2}>
         <Typography variant="h2" fontSize={18}>
           EN
         </Typography>
